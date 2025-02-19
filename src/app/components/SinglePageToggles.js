ba12ca7a88a92c7b24270005531c586c548fb6f7
Dimitar Pashovski <dipa@boozt.com>
2025-02-19 19:22:30 +0100

    Add single page toggles

---
+++ src/app/components/SinglePageToggles.js
@@ -0,0 +1,19 @@
+/** @jsxImportSource @emotion/react */
+import styled from '@emotion/styled';
+import * as React from 'react';
+import { StopwatchButton } from '../components';
+
+const SinglePageTogglesWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`;
+
+export default function SinglePageToggles() {
+    return (
+        <SinglePageTogglesWrapper>
+            <StopwatchButton>Lap</StopwatchButton>
+            <StopwatchButton theme='danger'>Stop</StopwatchButton>
+        </SinglePageTogglesWrapper>
+    )
+}
--- src/app/components/index.js
+++ src/app/components/index.js
@@ -1,5 +1,6 @@
 export { default as StopwatchesList } from './StopwatchesList';
 export { default as StopwatchButton } from './StopwatchButton';
 export { default as MainCounter } from './MainCounter';
+export { default as SinglePageToggles } from './SinglePageToggles';
 export { default as AppWrapper } from './AppWrapper';
 export { default as AppMainArea } from './AppMainArea';
